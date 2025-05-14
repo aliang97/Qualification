59cbb8577f2e7ffc28515a038ac050625a0150bb
Alex Liang <notmyworkemail3@gmail.com>
2025-05-14 01:22:18 -0400

    change repo name to 'Resume'

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Qualification
+# Resume
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Qualification',
+  base: '/Resume',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
